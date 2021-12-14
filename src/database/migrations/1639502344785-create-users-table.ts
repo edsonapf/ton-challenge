36e21c5b2fc88b36ae034c15c0638891fff2c6ef
Edson Alves <edsonapf@gmail.com>
2021-12-14 16:44:25 -0300

    feat: add migration and user entity

---
+++ src/database/migrations/1639502344785-create-users-table.ts
@@ -0,0 +1,34 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class createUsersTable1639502344785 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+
+    await queryRunner.createTable(new Table({
+      name: 'users',
+      columns: [
+        {
+          name: 'id',
+          type: 'uuid',
+          isPrimary: true,
+          isUnique: true,
+          generationStrategy: 'uuid',
+          default: 'uuid_generate_v4()',
+        },
+        {
+          name: 'name',
+          type: 'varchar',
+        },
+        {
+          name: 'email',
+          type: 'varchar',
+          isUnique: true,
+        },
+      ],
+    }));
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('DROP TABLE users');
+  }
+}

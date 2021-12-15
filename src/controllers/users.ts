5c210a1c2a9ace13226f0cba5046d08245fa2117
Edson Alves <edsonapf@gmail.com>
2021-12-15 11:59:55 -0300

    feat(users.ts): handle database errors

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -6,40 +6,50 @@ class UsersController {
   constructor(private readonly usersRepository: UsersRepository) {}
 
   findUserById = async (request: Request, response: Response) => {
-    const fieldsValidation = validationResult(request);
-    if (!fieldsValidation.isEmpty()) {
-      return response.status(400).json({ errors: fieldsValidation.array() });
-    }
+    try {
+      const fieldsValidation = validationResult(request);
+      if (!fieldsValidation.isEmpty()) {
+        return response.status(400).json({ errors: fieldsValidation.array() });
+      }
 
-    const { id } = request.params;
-    const userFound = await this.usersRepository.findUserById(id);
-    if (!userFound) {
-      return response.status(404).json({ error: 'User not found' });
-    }
+      const { id } = request.params;
+      const userFound = await this.usersRepository.findUserById(id);
+      if (!userFound) {
+        return response.status(404).json({ error: 'User not found' });
+      }
 
-    return response.json(userFound);
+      return response.json(userFound);
+    } catch (exception) {
+      console.log({ exception });
+      return response.status(500).json({ errors: 'Internal server error' });
+    }
   };
 
   createUser = async (request: Request, response: Response) => {
-    const fieldsValidation = validationResult(request);
-    if (!fieldsValidation.isEmpty()) {
-      return response.status(400).json({ errors: fieldsValidation.array() });
+    try {
+      const fieldsValidation = validationResult(request);
+      if (!fieldsValidation.isEmpty()) {
+        return response.status(400).json({ errors: fieldsValidation.array() });
+      }
+
+      const { name, email } = request.body;
+
+      if (!name) {
+        return response.status(400).json({ error: 'Field name is required' });
+      }
+
+      const userByEmailExists = await this.usersRepository.findUserByEmail(email);
+      if (userByEmailExists) {
+        return response.status(400).json({ error: 'User with this email already exists' });
+      }
+
+      const newUser = await this.usersRepository.createUser(name, email);
+
+      return response.json(newUser);
+    } catch (exception) {
+      console.log({ exception });
+      return response.status(500).json({ errors: 'Internal server error' });
     }
-
-    const { name, email } = request.body;
-
-    if (!name) {
-      return response.status(400).json({ error: 'Field name is required' });
-    }
-
-    const userByEmailExists = await this.usersRepository.findUserByEmail(email);
-    if (userByEmailExists) {
-      return response.status(400).json({ error: 'User with this email already exists' });
-    }
-
-    const newUser = await this.usersRepository.createUser(name, email);
-
-    return response.json(newUser);
   };
 }
 
